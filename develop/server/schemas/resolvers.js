153420059d385ae741cdd42112f76525c26199e0
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-04-30 15:17:55 -0400

    added all typedefs, queries and mutations

--- develop/server/schemas/resolvers.js
+++ develop/server/schemas/resolvers.js
@@ -3,13 +3,6 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express'); 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username });
-    },
-
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
@@ -18,36 +11,25 @@ const resolvers = {
     }
   },
   Mutation: {
-    addUser: async (_, { username, email, password }) => {
-      try {
-        const user = await User.create({ username, email, password });
-    
-        if (!user) {
-          throw new Error('Failed to create user');
-        }
-    
-        console.log('New user created:', user);
-        return { token: signToken(user), user };
-      } catch (error) {
-        console.error('Error creating user:', error);
-        throw new Error('Failed to create user');
-      }
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw AuthenticationError;
+        throw AuthenticationError
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw AuthenticationError;
+        throw AuthenticationError
       }
 
       const token = signToken(user);
-
       return { token, user };
     },
   }
