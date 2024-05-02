d5948ac52bd3ba3ef8825eda7cfdc2be37d6e17b
Neglon <thomasfneylon@gmail.com>
2024-05-02 18:39:48 -0400

    login update

--- develop/client/src/pages/Profile.jsx
+++ develop/client/src/pages/Profile.jsx
@@ -43,7 +43,7 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {user.username}'s` profile.
         </h2>
       </div>
     </div>
