d8c1499a6c4b5f50d90de89a4d7682bfb049a8a3
Neglon <thomasfneylon@gmail.com>
2024-05-02 21:20:33 -0400

    dynamic header
    header loads if logged in

--- develop/client/src/components/Header.jsx
+++ develop/client/src/components/Header.jsx
@@ -10,24 +10,16 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-       
+        <h1>TESTING</h1>
         <div>
-          {Auth.loggedIn() ? (
+          
             <>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
             </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/">
-                Signup
-              </Link>
-            </>
-          )}
+          
+          
         </div>
       </div>
     </header>
