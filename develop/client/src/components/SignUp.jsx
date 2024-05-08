cd13393c887145b3a7e255d30e213497f5ad1571
HeidiHarmony <tenibrae@gmail.com>
2024-05-07 23:50:59 -0400

    Working on landing page layout and integrating sing up and log in styles

--- develop/client/src/components/SignUp.jsx
+++ develop/client/src/components/SignUp.jsx
@@ -39,10 +39,10 @@ const Signup = () => {
   };
 
   return (
-    <main className="">
+
       <div className="signup-container">
         <div className="">
-          <h4 className="signup-header"><span className='italic'>NEW?</span> START HERE</h4>
+          <h4 className="signup-header"><span className='italic'>NEW? </span>START&nbsp;HERE!</h4>
           <h3 className='signup-text'>CREATE AN ACCOUNT</h3>
           <div className="">
             {data ? (
@@ -94,7 +94,6 @@ const Signup = () => {
           </div>
         </div>
       </div>
-    </main>
   );
 };
 
