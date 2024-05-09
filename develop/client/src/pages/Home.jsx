c325e7dcaa34504f97ba440bb12a5764d89924e8
HeidiHarmony <tenibrae@gmail.com>
2024-05-08 23:33:19 -0400

    Working on plant component. Inserted reference temporarily into home.jsx to see if it would render but couldn't fully test because there was no plant id

--- develop/client/src/pages/Home.jsx
+++ develop/client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import '../App.css';
 import './Home.css';
 import Signup from '../components/SignUp';
 import Login from '../components/Login';
+// Temp for testing, actually belongs on the dashboard
+import Plant from '../components/Plant';
 import logo from '../assets/images/plantplanner-square-0.5x.png';
 import landingBG from '../assets/images/plant-planner-landingBG.png';
 
@@ -41,32 +43,29 @@ function Home() {
           <h1 className="headline">The secret to a happy and healthy garden?<span className="pop">Remembering&nbsp;to&nbsp;take&nbsp;care&nbsp;of&nbsp;your&nbsp;plants!</span></h1>
         </div>
   
-      <div className="w3-container test"> 
+        <div className="w3-container test"> 
 
-        <div className="promo-blurb w3-left-align w3-col l4 m12 w3-padding test">
-          <h2>Turn your <b>black thumb</b> <span className="pop">green</span> with Forget-Me-Not Plant Planner</h2> 
-          <p>Keep all the information for your indoor and outdoor plants in one place:</p>
-          <ul className="w3-ul ulPadding">
-            <li>Plant names and photos</li>
-            <li>Watering schedules</li>
-            <li>Notes about your plants</li>
-            <li>And more!</li>
-          </ul>
+          <div className="promo-blurb w3-left-align w3-col l4 m12 w3-padding test">
+          < Plant/>
+            <h2>Turn your <b>black thumb</b> <span className="pop">green</span> with Forget-Me-Not Plant Planner</h2> 
+            <p>Keep all the information for your indoor and outdoor plants in one place:</p>
+            <ul className="w3-ul ulPadding">
+              <li>Plant names and photos</li>
+              <li>Watering schedules</li>
+              <li>Notes about your plants</li>
+              <li>And more!</li>
+            </ul> 
+          </div>
+
         </div>
 
         <div className="landing-logo w3-col l9 m6 test">
             <img src={logo} alt="Forget-Me-Not Plant Planner" />
         </div>
 
-      </div>
-
     </div>
 
     <div className="smaller w3-quarter w3-display-container w3-padding-16 test">
-
-{/*       <div className="watering w3-col l12">
-        <img src={watering} alt="watering can" />
-      </div> */}
 
       <div style={authContainerStyle}>
           <div style={authStyle}>
