bb63b75a43d37423dec6a392e1c88970e626373c
HeidiHarmony <tenibrae@gmail.com>
2024-05-09 23:05:51 -0400

    Added logo to navbar component, added footer art, added dashboard page

--- develop/client/src/pages/dashboard..jsx
+++ develop/client/src/pages/dashboard..jsx
@@ -1,33 +1,33 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href=App.css>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Forget-Me-Not Plant Planner</title>
-  </head>
-  <body>
-    <div id="root" class="w3-container">
-      <!--dashboard components begin here-->
-      <!--header, garden, my-tasks-->
+import { useState } from 'react';
+import '../App.css';
+import './Home.css';
+import Plant from '../components/Plant';
+import Garden from '../components/Garden';
+import Navbar from '../components/Navbar';
+import footerGround from '../assets/images/footerGround.png';
+// import the tasklist component
+// import TaskList from '../components/TaskList';
 
-    <header class="w3-container w3-sand">
-      <h1>Forget-Me-Not Plant Planner</h1>
-      <p>Test</p>
+function Dashboard() {
+  const [count, setCount] = useState(0)
+
+  return (
+<>
+    <header className="w3-container w3-sand">
+      <Navbar />
     </header>
 
-    <main class="w3-row w3-row-padding">
+    <main className="w3-row w3-row-padding">
 
-      <div id="garden" class="w3-container w3-col s4 m6 l8 w3-cyan">
-        <h2>Garden</h2>
-        <p>Test</p>
+      <div id="garden" className="w3-container w3-col s4 m6 l8">
+        <Garden />
      </div>
 
-      <div class="w3-container w3-col s8 m6 l4"> <!--Holds my-tasks-->
+      <div className="w3-container w3-col s8 m6 l4"> 
 
-        <div id="my-tasks" class="w3-brown">
-          <h2>My Tasks</h2>
+        <div id="my-tasks" className="w3-brown myTasksStyle">
+          {/* Replace with myTasks component */}
+          <h2>My Tasks Go Here</h2>
           <p>Test</p>
         </div>
 
@@ -35,12 +35,10 @@
 
     </main>
 
-    <footer class="w3-container w3-pale-green">
-      <h5>Footer</h5>
-      <p>Brought to you by Project 3 Superstars</p>
+    <footer className="w3-container w3-pale-green">
+      <img src="{footerGround}" alt="footer ground" className="footerStyle"/>
+      <p className="footerTextStyle">Brought to you by Project 3 Superstars</p>
     </footer>
-    
-    </div> <!--End of Root-->
-    <!--<script type="module" src="/src/main.jsx"></script>-->
-  </body>
-</html>
+    </>
+  );
+}
