8f162a4ea5d7b278dcf8063793f57e1caa712d4c
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-05-10 22:57:28 -0400

    removed code in plants list

--- develop/client/src/components/PlantList/index.jsx
+++ develop/client/src/components/PlantList/index.jsx
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useApolloClient } from '@apollo/client';
+import { useApolloClient } from '@apollo/client'; // Import useApolloClient
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_SINGLE_PLANT } from '../../utils/queries';
 import { REMOVE_PLANT } from '../../utils/mutations';
 import { QUERY_MYPLANTS, QUERY_ME } from '../../utils/queries';
-import Plants from '../../assets/images/plant.jpg';
-import { usePlant } from '../../utils/GlobalState'; 
+import Plants from '../../assets/images/plant.jpg'
 
 const PlantList = ({ plants, onClick }) => {
-  const client = useApolloClient();
+  const client = useApolloClient(); // Initialize Apollo Client
+
   const { refetch: refetchPlants } = useQuery(QUERY_MYPLANTS);
   const { refetch: refetchMe } = useQuery(QUERY_ME);
   const [removePlantMutation] = useMutation(REMOVE_PLANT);
 
-  const { plantsColor, setPlantsColor } = usePlant();
-
-  const [plantsList, setPlantsList] = useState(plants); 
-
   const handleDeletePlant = async (plantId) => {
     try {
       await removePlantMutation({ variables: { plantId } });
+      // Refetch notes and user information after deletion to update the lists
       refetchPlants();
       refetchMe();
     } catch (error) {
@@ -28,45 +24,31 @@ const PlantList = ({ plants, onClick }) => {
   };
 
   const handlePlantClick = async (plantId) => {
+    console.log('Clicked plantId:', plantId); // Add this line for debugging
+
     try {
       const { data } = await client.query({
         query: QUERY_SINGLE_PLANT,
         variables: { id: plantId },
       });
-      onClick(data.plant);
+      onClick(data.plant); // Call onClick with the plant data
     } catch (error) {
       console.error('Error fetching plant data:', error);
     }
   };
 
-  useEffect(() => {
-    setPlantsList(plants); 
-  }, [plants]);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setPlantsColor(false); // Change to false after 10 seconds
-    }, 10000);
-
-    return () => clearTimeout(timeout);
-  }, [setPlantsColor]);
+  if (!plants.length) {
+    return <h3>No Plants Yet</h3>;
+  }
 
   return (
     <div>
       <h3>Plants</h3>
-      {plantsList.map((plant) => ( 
-        <div
-          key={plant._id}
-          className="card"
-          onClick={() => handlePlantClick(plant._id)}
-        >
+      {plants.map((plant) => (
+        <div key={plant._id} className="card" onClick={() => handlePlantClick(plant._id)}>
           <div className="card-body bg-light p-2">
             <p>Name: {plant.name}</p>
-            <img
-              src={Plants}
-              alt={plant.name}
-              style={{ border: `7px solid ${plantsColor ? 'green' : 'orange'}` }}
-            />
+            <img src={Plants} alt={plant.name} />
             <p>Instructions: {plant.wateringTask.instructions}</p>
             <button className="btn btn-danger" onClick={() => handleDeletePlant(plant._id)}>
               DELETE PLANT
