43182520fb49521c559c5366c3e520ba5f1b9d01
Neglon <thomasfneylon@gmail.com>
2024-05-13 14:08:36 -0400

    custom plant toggle

--- develop/client/src/components/Dropdown.jsx
+++ develop/client/src/components/Dropdown.jsx
@@ -3,11 +3,14 @@ import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ALL_PLANTS } from '../utils/queries'; 
 import { ADD_PLANT_TO_USER } from '../utils/mutations';  
 import { useToggle } from '../utils/ToggleContext';
+import AddPlantForm from './PlantForm/index'
+import './Dropdown.css';
 
 function Dropdown({ userId }) {
   const { loading, error, data } = useQuery(QUERY_ALL_PLANTS);
   const [selectedItem, setSelectedItem] = useState('');
   const [confirm, setConfirm] = useState(false);
+  const [showAddPlantForm, setShowAddPlantForm] = useState(false);
   const [addPlantToUser] = useMutation(ADD_PLANT_TO_USER);
   const { hideDropdownMenu } = useToggle();
 
@@ -37,6 +40,10 @@ function Dropdown({ userId }) {
     }
   };
 
+  const toggleAddPlantForm = () => {
+    setShowAddPlantForm(!showAddPlantForm);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading plants data: {error.message}</p>;
   if (data && data.allPlants.length === 0) return <p>No plants available to select.</p>;
@@ -51,12 +58,18 @@ function Dropdown({ userId }) {
           </option>
         ))}
       </select>
+      <hr />
       {selectedItem && (
         <button onClick={handleConfirm} disabled={confirm}>
           Confirm
         </button>
       )}
-      <button onClick={hideDropdownMenu}>Hide</button>
+      <hr />
+      <button onClick={hideDropdownMenu}>Cancel</button>
+      <hr />
+      <button onClick={toggleAddPlantForm}>Add Custom Plant</button>
+      <button onClick={hideDropdownMenu}>Cancel</button>
+      {showAddPlantForm && <AddPlantForm toggleForm={toggleAddPlantForm} />}
     </div>
   );
 }
