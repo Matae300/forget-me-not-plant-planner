c71f99913e9f80a05e92beef455ebb2ed0a01a96
HeidiHarmony <tenibrae@gmail.com>
2024-05-13 17:30:32 -0400

    Styling tweaks

--- develop/client/src/main.jsx
+++ develop/client/src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className='display-2'>Problem displaying page!</h1>,
     children: [
       {
         index: true,
